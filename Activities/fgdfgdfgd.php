77198e5bae661666263e4b737408a77372b2b88b
ASAD <kasad@ggc.edu>
2017-03-13 04:11:49 -0400

    HTML/CSS

---
+++ Activities/fgdfgdfgd.php
@@ -0,0 +1,154 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>
+Activity 0315
+</title>
+<link rel="stylesheet"
+href="lab5.css">
+</head>
+<body>
+
+
+<table border=5 style=text-align:left;>
+
+
+<tr>
+        <td COLSPAN=5 class=boxtop style=text-align:center;>Learn how to do web design
+		<img src="Penguins.jpg" style="width:50px;height:50px;">
+		</td>
+		</tr>
+        <tr>
+            <td rowspan=3 colspan=2 class=boxgg>This is the navigation bar with a background image</td>
+          
+            <td colspan=5 class=boxhh>This is header</td>
+        </tr>
+		
+		
+		
+        <tr>
+            <td colspan=2 class=boxee>This where your main content is placed
+<table class=table6 border=0   >
+
+		<th>Month</th>
+		<th>Electricty</th>
+		<th>Water & Sewer</th>
+		<th>Gas</th>
+		<th>Total</th>
+		</tr>
+		
+		<tr>
+		<td>January</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		<td rowspan=14>3900</td>
+		</tr>
+		
+		<tr>
+		<td>February</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>March</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>April</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>May</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>June</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>July</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		
+		</tr>
+		
+		<tr>
+		<td>August</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>September</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>October</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>November</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<td>December</td>
+		<td>100</td>
+		<td>50</td>
+		<td>175</td>
+		</tr>
+		
+		<tr>
+		<th>Total</th>
+		<th>1200</th>
+		<th>600</th>
+		<th>2100</th>
+		</tr>
+		
+</table>
+
+
+			
+			
+			
+			</td>
+        </tr>
+
+        
+		<tr>
+		<td COLSPAN=5 class=boxbot>This is your footer</td>
+      
+		</tr>
+		
+		</table>
+
+
+
+
+</body>
+</html>
---
+++ Activities/welcomeme.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+</head>
+<body>
+<p>
+<?php echo "welcome"?> &nbsp
+<?php echo $_POST["email"]; ?>
+</p>
+
+
+
+
+
+
+</body>
+</html>
